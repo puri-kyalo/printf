ee54d749ee6744c6e694af9561db38dc8734dcc8
kwame-asa <132373937+kwame-asa@users.noreply.github.com>
2023-07-25 08:15:06 +0000

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,64 +1,37 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
+ * get_precision - write a function that calculates the precision for printing.
+ * @format: The formatted string in which to print the arguments.
+ * @i: is the list of arguments to be printed.
+ * @list: is the list of arguments.
  * Return: Precision.
  */
-#include "main.h"
-
-/************************* WRITE HANDLE *************************/
-/**
- * handle_write_char - Prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
- *
- * Return: Number of chars printed.
- */
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
-	int i = 0;
-	char padd = ' ';
-int get_size(const char *format, int *i)
+int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int size = 0;
+int curr_i = *i + 1;
+int precision = -1;
 
-	if (format[curr_i] == 'l')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
+if (format[curr_i] != '.')
+return (precision);
 
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
-
-	return (size);
+precision = 0;
+for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+{
+if (is_digit(format[curr_i]))
+{
+precision *= 10;
+precision += format[curr_i] - '0';
 }
-#include "main.h"
-
-/************************* WRITE HANDLE *************************/
-/**
- * handle_write_char - Prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
- *
- * Return: Number of chars printed.
- */
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
-	int i = 0;
-	char padd = ' ';
+else if (format[curr_i] == '*')
+{
+curr_i++;
+precision = va_arg(list, int);
+break;
+}
+else
+break;
+}
+*i = curr_i - 1;
+return (precision);
+}
